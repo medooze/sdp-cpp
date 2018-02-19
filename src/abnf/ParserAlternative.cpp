6ed8300ea560cc2dfe6b9cae5ef316a6ccdcb5c2
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-02-19 17:14:32 +0100

    Add missing file

---
+++ src/abnf/ParserAlternative.cpp
@@ -0,0 +1,99 @@
+/* -----------------------------------------------------------------------------
+ * ParserAlternative.cpp
+ * -----------------------------------------------------------------------------
+ *
+ * Producer : com.parse2.aparse.Parser 2.5
+ * Produced : Mon Jan 08 13:30:55 CET 2018
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+#include "ParserAlternative.hpp"
+
+using namespace abnf;
+
+ParserAlternative::ParserAlternative(unsigned int start) :
+start(start), end(start)
+{
+}
+
+ParserAlternative::ParserAlternative(const ParserAlternative& alternative) :
+start(alternative.start), end(alternative.end)
+{
+  vector<const Rule*>::const_iterator r;
+
+  for (r = alternative.rules.begin(); r != alternative.rules.end(); r++)
+    this->rules.push_back((*r)->clone());
+}
+
+ParserAlternative& ParserAlternative::operator=(const ParserAlternative& alternative)
+{
+  if (&alternative != this)
+  {
+    start = alternative.start;
+    end = alternative.end;
+
+    vector<const Rule*>::const_iterator r;
+
+    for (r = rules.begin(); r != rules.end(); r++)
+      delete *r;
+
+    rules.empty();
+
+    for (r = alternative.rules.begin(); r != alternative.rules.end(); r++)
+      rules.push_back((*r)->clone());
+  }
+  return *this;
+}
+
+ParserAlternative::~ParserAlternative()
+{
+  vector<const Rule*>::const_iterator r;
+
+  for (r = rules.begin(); r != rules.end(); r++)
+    delete *r;
+}
+
+void ParserAlternative::add(const Rule& rule, unsigned int end)
+{
+  rules.push_back(rule.clone());
+  this->end = end;
+}
+
+void ParserAlternative::add(const std::vector<const Rule*>& rules, unsigned int end)
+{
+  vector<const Rule*>::const_iterator r;
+
+  for (r = rules.begin(); r != rules.end(); r++)
+    this->rules.push_back((*r)->clone());
+
+  this->end = end;
+}
+
+const ParserAlternative* ParserAlternative::getBest(vector<const ParserAlternative*> alternatives)
+{
+  vector<const ParserAlternative*>::const_iterator a;
+
+  const ParserAlternative* best = NULL;
+
+  for (a = alternatives.begin(); a != alternatives.end(); a++)
+  {
+    if (best == NULL || (*a)->end > best->end)
+    {
+      best = *a;
+    }
+  }
+
+  return best;
+}
+
+/* -----------------------------------------------------------------------------
+ * eof
+ * -----------------------------------------------------------------------------
+ */
---
+++ src/abnf/ParserAlternative.hpp
@@ -0,0 +1,47 @@
+/* -----------------------------------------------------------------------------
+ * ParserAlternative.hpp
+ * -----------------------------------------------------------------------------
+ *
+ * Producer : com.parse2.aparse.Parser 2.5
+ * Produced : Mon Jan 08 13:30:55 CET 2018
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef ParserAlternative_hpp
+#define ParserAlternative_hpp
+
+#include <string>
+#include <vector>
+
+#include "Rule.hpp"
+
+namespace abnf {
+
+class ParserAlternative
+{
+public:
+  std::vector<const Rule*> rules;
+  unsigned int start;
+  unsigned int end;
+
+  ParserAlternative(unsigned int start);
+  ParserAlternative(const ParserAlternative& alternative);
+
+  ParserAlternative& operator=(const ParserAlternative& alternative);
+
+  ~ParserAlternative();
+
+  void add(const Rule& rule, unsigned int end);
+  void add(const std::vector<const Rule*>& rules, unsigned int end);
+
+  static const ParserAlternative* getBest(vector<const ParserAlternative*> alternatives);
+};
+
+}
+
+#endif
+/* -----------------------------------------------------------------------------
+ * eof
+ * -----------------------------------------------------------------------------
+ */
